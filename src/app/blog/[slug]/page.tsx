cb960e0e086f6a74fed807a62eed5be375e7c829
Kidist Samuel <kidistsamuel21@gmail.com>
2025-04-28 21:13:41 +0300

    throw notFound 404 if no post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,41 +2,31 @@ import Layout from '@/app/components/Layout';
 import React from 'react';
 import blogPosts from '../../data/blogPosts';
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-// Define only the slug in the params
-interface BlogPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-// Generate static paths with only slugs
+// Generate static paths
 export async function generateStaticParams() {
   return blogPosts.map(blogPost => ({
     slug: blogPost.slug,
   }));
 }
 
-// Component that fetches the blog post based on the slug
-const BlogPost = ({ params }: BlogPageProps) => {
+// Page component
+export default function BlogPost({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const post = blogPosts.find(p => p.slug === slug);
 
+  if (!post) {
+    notFound(); // throw 404 if no post
+  }
+
   return (
     <Layout>
-      {!post ? (
-        <div>Blog post not found</div>
-      ) : (
-        <>
-          <Link href="/blog" className="text-blue-500 hover:underline mt-4 block">
-            ← Back to Blog
-          </Link>
-          <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-          <p className="text-gray-700">{post.content}</p>
-        </>
-      )}
+      <Link href="/blog" className="text-blue-500 hover:underline mt-4 block">
+        ← Back to Blog
+      </Link>
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <p className="text-gray-700">{post.content}</p>
     </Layout>
   );
-};
-
-export default BlogPost;
+}
