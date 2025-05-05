bd0f9fb888f58e31d6964b55e5ae4cbc65198f3c
Kidist Samuel <kidistsamuel21@gmail.com>
2025-05-05 19:15:21 +0300

    restore blog slug

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import Layout from '@/app/components/Layout';
+import React from 'react';
+import blogPosts from '../../data/blogPosts';
+import Link from 'next/link';
+
+// Define the type for the params
+interface BlogPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+// Generate static paths with only slugs
+export async function generateStaticParams() {
+  return blogPosts.map(blogPost => ({
+    slug: blogPost.slug,
+  }));
+}
+
+// Component that fetches the blog post based on the slug
+const BlogPost = ({ params }: BlogPageProps) => {
+  const { slug } = params;
+  const post = blogPosts.find(p => p.slug === slug);
+
+  return (
+    <Layout>
+      {!post ? (
+        <div>Blog post not found</div>
+      ) : (
+        <>
+          <Link href="/blog" className="text-blue-500 hover:underline mt-4 block">
+            ← Back to Blog
+          </Link>
+          <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+          <p className="text-gray-700">{post.content}</p>
+        </>
+      )}
+    </Layout>
+  );
+};
+
+export default BlogPost;
